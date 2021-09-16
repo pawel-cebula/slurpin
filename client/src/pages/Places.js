65097b97564cf1418531e094a6146397d4a1b1bb
Pawel Cebula <ptcebula@gmail.com>
2021-09-16 17:33:47 +0200

    feat: Enable sorting Places by number of checkins desc and asc

--- client/src/pages/Places.js
+++ client/src/pages/Places.js
@@ -1,19 +1,57 @@
-import React from 'react';
+import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
+import { Radio } from 'antd';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import Place from '../components/Place';
 import Spinner from '../components/Spinner';
 
 const Places = () => {
-  const places = useSelector((state) => state.places);
+  const placesState = useSelector((state) => state.places);
+  const [places, setPlaces] = useState(placesState.data);
+  const [sort, setSort] = useState('desc');
+
+  useEffect(() => {
+    const sortByCheckins = (array, type) =>
+      type === 'desc'
+        ? [...array].sort((a, b) => b.checkins.length - a.checkins.length)
+        : [...array].sort((a, b) => a.checkins.length - b.checkins.length);
+    if (placesState.data) {
+      setPlaces(sortByCheckins(placesState.data, sort));
+    }
+  }, [placesState, sort]);
+
+  const onChange = (e) => {
+    setSort(e.target.value);
+  };
 
   return (
     <div>
       <h1>Ramen places in Berlin</h1>
-      {places.isLoading && <Spinner />}
-      {places.data &&
-        places.data
-          .sort((a, b) => b.checkins.length - a.checkins.length)
-          .map((place) => <Place key={place.id} place={place} />)}
+      {placesState.isLoading && <Spinner />}
+      {places && (
+        <div>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              color: '#bfbfbf',
+            }}
+          >
+            Sort by number of checkins:&nbsp;
+            <Radio.Group onChange={onChange} value={sort}>
+              <Radio value="desc">
+                <ArrowDownOutlined />
+              </Radio>
+              <Radio value="asc">
+                <ArrowUpOutlined />
+              </Radio>
+            </Radio.Group>
+          </div>
+          {places.map((place) => (
+            <Place key={place.id} place={place} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
