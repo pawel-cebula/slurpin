1ed93fdbabd20af92fe6e051706f3dfe616a2a89
Pawel Cebula <ptcebula@gmail.com>
2021-09-29 17:28:46 +0200

    fix: Serve index.html from build on all server paths

--- app.js
+++ app.js
@@ -16,7 +16,10 @@ const {
 const authRouter = require('./routes/auth');
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(
+    '/*',
+    express.static(path.join(__dirname, 'client/build', 'index.html'))
+  );
 }
 
 if (process.env.NODE_ENV === 'development') {
